39f609ea5284260f5781b1efe9d1b871c5faf1f5
ItsDiegoTBG <>
2024-10-23 14:16:10 -0500

    Inicio

---
+++ Taller3/src/main/java/com/mycompany/taller3/Usuario.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller3;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Usuario {
+    protected String usuario;
+    protected String contraseña;
+    protected String nombre;
+    protected String apellido;
+    
+    protected boolean logIn(){
+        return true;
+    }
+}
